c7caa459aaa3923fbb89ee2062981ff2f2a24512
Jasper Chan <jasperchan515@gmail.com>
2020-04-03 18:42:50 -0700

    Add nested navigator for status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'statusbar.dart';
 
 void main() {
@@ -7,12 +9,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => MyHomePage(title: "home"),
+      }
     );
   }
 }
@@ -39,6 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   double _width = 0;
   double _height = 0;
+  final navigatorKey = GlobalKey<NavigatorState>();
 
   void _incrementCounter() {
     setState(() {
@@ -64,19 +71,36 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(20.0),
-        child: StatusBar()
+        child: StatusBar(),
         ),
-      body: ListView.builder(
-        itemBuilder: (context, index) => ListTile(
-          title: FocusableText(
-            index.toString(),
-            autofocus: index == 0,
-          ),
-        ),
-        itemCount: 50,
+      body: Navigator(
+        initialRoute: 'menu',
+        onGenerateRoute: (RouteSettings settings) {
+          WidgetBuilder builder;
+          builder = (BuildContext context) => PiPodListView();
+          return CupertinoPageRoute(builder: builder, settings: settings);
+        }
       ),
     );
   }
+}
+
+class PiPodListView extends StatelessWidget {
+  const PiPodListView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemExtent: 20.0,
+      itemBuilder: (context, index) => FocusableText(
+          index.toString(),
+          autofocus: index == 0,
+      ),
+      itemCount: 10,
+    );
+  }
+
 }
 
 class FocusableText extends StatelessWidget {
@@ -85,14 +109,47 @@ class FocusableText extends StatelessWidget {
   final String data;
   final bool autofocus;
 
+  bool _onKeyPressed(FocusNode node, RawKeyEvent event, BuildContext context) {
+    if (event is RawKeyDownEvent) {
+      // print("Got button press");
+      // print(event.toString());
+      if(event.logicalKey == LogicalKeyboardKey.enter) {
+        print("$data selected");
+        Navigator.of(context)
+          .pushReplacementNamed('menu');
+        return true;
+      } else if (event.logicalKey == LogicalKeyboardKey.escape) {
+        print("Escaping");
+        Navigator.of(context).popAndPushNamed('menu');
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+
+  bool _onFocusChanged(bool gotFocus) {
+    // if(gotFocus)
+    //   print("$data got focus");
+    // else
+    //   print("$data lost focus");
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Focus(
+      onKey: (node, event) => _onKeyPressed(node, event, context),
+      onFocusChange: _onFocusChanged,
       autofocus: autofocus,
       child: Builder(builder: (BuildContext context) {
-        return Container(
-          color: Focus.of(context).hasPrimaryFocus ? Colors.blue : null,
-          child: Text(data),
+        final FocusNode focusNode = Focus.of(context);
+        final bool hasFocus = focusNode.hasFocus;
+        return GestureDetector(
+          child: Container(
+            color: hasFocus ? Colors.blue : null,
+            child: Text(data),
+          ),
         );
       }),
     );
